218a9ec523fa38efb0da3df77c684771fe285646
Lisa Long <garrisonridge@gmail.com>
2017-09-28 10:17:41 -0400

    Add content for owl carousel to index.html

--- assets/src/js/app.js
+++ assets/src/js/app.js
@@ -5,18 +5,6 @@
 //= include ../../../bower_components/jquery/dist/jquery.js
 //= include ../../../bower_components/jquery.fitvids/jquery.fitvids.js
 
-$(document).ready(function () {
-
-//snippet from https://codepen.io/mel/pen/jLEKH
-    $('#firstWord').html(function () {
-        var text = $(this).text().trim().split('');
-        var first = text.shift();
-        return (text.length > 0 ? "<span class='bold'>" + first + '</span>' : first) + text.join('');
-    });
-
-});
-
-
 /* jQuery(function ($) {
 
     var site = new SiteController($);
